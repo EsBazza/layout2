f4cd4b2dfed57f9b87096b90c72830f93a6bf912
EsBazza <alonzoamarojuno@gmail.com>
2025-09-18 16:06:02 +0800

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+function HeaderLeft() {
+  return <div className="headerleft">{}</div>;
+}
+
+function HeaderCenter1({ name }) {
+  return <div className="headercenter1">{name}</div>;
+}
+
+function HeaderCenter2({ name }) {
+  return <div className="headercenter2">{name}</div>;
+}
+
+function HeaderCenterInner() {
+  return (
+    <div className="headercenter-inner">
+      <HeaderCenter1 name="JUNO" />
+      <HeaderCenter2 name="ALONZO" />
+    </div>
+  );
+}
+
+function HeaderCenter() {
+  return (
+    <div className="headercenter">
+      <HeaderCenterInner />
+    </div>
+  );
+}
+
+function HeaderRight() {
+  return <div className="headerright">{}</div>;
+}
+
+function Header() {
+  return (
+    <div className="headermain">
+      <HeaderLeft />
+      <HeaderCenter />
+      <HeaderRight />
+    </div>
+  );
+}
+
+
+function LeftContainer() {
+  return <div className="leftcontainer">{}</div>;
+}
+
+function RightContainer1() {
+  return <div className="rightcontainer1">{}</div>;
+}
+
+function RightContainer2() {
+  return <div className="rightcontainer2">{}</div>;
+}
+
+function RightContainer() {
+  return (
+    <div className="rightcontainer">
+      <RightContainer1 />
+      <RightContainer2 />
+    </div>
+  );
+}
+
+function MainContent() {
+  return (
+    <div className="contentmain">
+      <LeftContainer />
+      <RightContainer />
+    </div>
+  );
+}
+
+function FooterLeft() {
+  return <div className="footerleft">{}</div>;
+}
+
+function FooterCenter1({course}) {
+  return <div className="footercenter1">{course}</div>;
+}
+
+function FooterCenter2({course}) {
+  return <div className="footercenter2">{course}</div>;
+}
+
+function FooterRight() {
+  return <div className="footerright">{}</div>;
+}
+
+function Footer() {
+  return (
+    <div className="footermain">
+      <FooterLeft />
+      <FooterCenter1 course={"C-PCIT9"}/>
+      <FooterCenter2 course={"IT2A"}/>
+      <FooterRight />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div class="maincontainer">
-      <div class="headermain">
-        <div class="headerleft"></div>
-        <div class="headercenter">
-          <div class="headercenter-inner">
-            <div class="headercenter1"></div>
-            <div class="headercenter2"></div>
-          </div>
-        </div>
-        <div class="headerright"></div>
-      </div>
-      <div class="contentmain">
-        <div class="leftcontainer"></div>
-        <div class="rightcontainer">
-          <div class="rightcontainer1"></div>
-          <div class="rightcontainer2"></div>
-        </div>
-      </div>
-      <div class="footermain">
-        <div class="footerleft"></div>
-        <div class="footercenter1"></div>
-        <div class="footercenter2"></div>
-        <div class="footerright"></div>
-      </div>
+    <div className="maincontainer">
+      <Header/>
+      <MainContent />
+      <Footer />
     </div>
   );
 }
